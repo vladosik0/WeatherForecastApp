65bbeae5586e6a7b42a56a09beb9292fad52b0d9
Vlad_Kamchatny <vlad.kamchatniy@gmail.com>
2024-03-07 18:40:21 +0200

    Add serializable classes

--- app/src/main/java/com/vladosik0/weather_forecast_app/main_screen/data/network/current_location_weather/Current.kt
+++ app/src/main/java/com/vladosik0/weather_forecast_app/main_screen/data/network/current_location_weather/Current.kt
@@ -1,5 +1,8 @@
 package com.vladosik0.weather_forecast_app.main_screen.data.network.current_location_weather
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Current(
     val condition: Condition,
     val is_day: Int,
--- app/src/main/java/com/vladosik0/weather_forecast_app/main_screen/data/network/current_location_weather/Location.kt
+++ app/src/main/java/com/vladosik0/weather_forecast_app/main_screen/data/network/current_location_weather/Location.kt
@@ -1,5 +1,8 @@
 package com.vladosik0.weather_forecast_app.main_screen.data.network.current_location_weather
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Location(
     val country: String,
     val localtime: String,
