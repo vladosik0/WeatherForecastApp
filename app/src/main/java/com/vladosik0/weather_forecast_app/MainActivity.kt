8fe1a3a738a3e7cda3e38ec6049255ca22e141c3
Vlad_Kamchatny <vlad.kamchatniy@gmail.com>
2024-03-07 15:38:11 +0200

    Add UI template for main screen

--- app/src/main/java/com/vladosik0/weather_forecast_app/MainActivity.kt
+++ app/src/main/java/com/vladosik0/weather_forecast_app/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.vladosik0.weather_forecast_app.ui.MainScreen
 import com.vladosik0.weather_forecast_app.ui.theme.WeatherForecastAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    MainScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/vladosik0/weather_forecast_app/ui/MainScreen.kt
@@ -0,0 +1,118 @@
+package com.vladosik0.weather_forecast_app.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vladosik0.weather_forecast_app.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text(stringResource(id = R.string.app_name)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                ),
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            Text(
+                text = stringResource(id = R.string.current_location_title),
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.padding(6.dp)
+            )
+            HorizontalDivider(thickness = 2.dp, modifier = Modifier.padding(6.dp))
+            LocationItem(locationName = "Kyiv", temperature = 5, weatherIcon = 116)
+            Text(
+                text = stringResource(id = R.string.favourite_places_title),
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.padding(6.dp)
+            )
+            HorizontalDivider(thickness = 2.dp, modifier = Modifier.padding(6.dp))
+            LocationItem(locationName = "London", temperature = 2, weatherIcon = 113)
+            LocationItem(locationName = "Kyiv", temperature = 5, weatherIcon = 116)
+            LocationItem(locationName = "New York", temperature = 7, weatherIcon = 119)
+            LocationItem(locationName = "Madrid", temperature = 8, weatherIcon = 260)
+            LocationItem(locationName = "Amsterdam", temperature = 10, weatherIcon = 296)
+            LocationItem(locationName = "Lisbon", temperature = 3, weatherIcon = 299)
+            LocationItem(locationName = "Munich", temperature = -2, weatherIcon = 122)
+        }
+    }
+}
+
+@Composable
+fun LocationItem(
+    locationName: String,
+    temperature: Int,
+    weatherIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+            .size(96.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+        shape = CardDefaults.elevatedShape
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = locationName,
+                modifier = Modifier.weight(1f),
+                fontSize = 24.sp
+            )
+            Text(
+                text = "$temperature°C",
+                fontSize = 24.sp
+            )
+            Image(
+                modifier = Modifier.size(64.dp),
+                painter = painterResource(id = R.drawable._113),
+                contentDescription = null
+            )
+        }
+    }
+
+}
